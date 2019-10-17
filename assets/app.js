fbd4180a7ede5dfe4ee2888896bacd793f892d54
Ashley Clark <debashley06@gmail.com>
2019-10-17 17:35:26 -0400

    works in the console log, not working in browser, remove button problems now

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,5 @@
+$(document).ready(function () {
+
 //Firebase
 var config = {
     apiKey: "AIzaSyCVT0Ays4I6djiYKfIL82tbx3037FpNZq4",
@@ -17,11 +19,9 @@ var database = firebase.database();
 // variables
   var trainName = "";
   var destination = "";
-  var freqMin = "";
-  var firstTrain = "";
-  var minsAway = "";
-  var tMinutes;
-  var tArrival;
+  var tFrequency;
+  var firstTrain;
+
 
 
 $("#submit-button").on("click", function (event) {
@@ -30,78 +30,83 @@ $("#submit-button").on("click", function (event) {
 //user inputs
       trainName = $("#trainInput").val().trim();
       destination = $("#destinationInput").val().trim();
-      freqMin = $("#freqInput").val().trim();
-      firstTrain = $("#firstTrainInput").val().trim();
-
+      tFrequency = $("#freqInput").val().trim();
+      firstTrain = $("#firstTrainInput").val().trim()
 //update variables with database
-     database.ref().push({
+      database.ref().push({
           trainName: trainName,
           destination: destination,
-          freqMin: freqMin,
+          tFrequency: tFrequency,
           firstTrain: firstTrain
-     });
+ });
 
-  });
+});
+
+
 
   //function runs on new child added
   database.ref().on("child_added", function(snapshot){
-      //console.log(snapshot.val());
 
-        var trainFrequencyData = snapshot.val().freqMin
-        console.log("Train Frequency", trainFrequencyData);
+      var firstTime = firstTrain;
+            console.log("first train", firstTime)
+    
+      var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+            console.log(firstTimeConverted, "First time Converted");
+  
+      // Current Time
+      var currentTime = moment(new Date());
+            console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+  
+      
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+            console.log("DIFFERENCE: " + diffTime);
+  
+      
+      var tRemainder = diffTime % tFrequency;
+            console.log(tRemainder);
+  
+      
+      var tMinutes = tFrequency - tRemainder;
+            console.log("MINUTES TILL TRAIN: " + tMinutes);
+  
+      
+      var nextTrain = moment().add(tMinutes, "minutes");
+            console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
 
-        var firstTrainData = snapshot.val().firstTrain
-
-              console.log("first train value", firstTrainData)
-
-        var firstTimeConverted = moment(firstTrainData, "hh:mm").subtract(1, "years");
-              console.log("firstTimeConvert", firstTimeConverted)
-        
-        var currentTime = moment();
-
-      // Difference between the times
-        var diffTime = moment().diff(moment(firstTrainData), "minutes");
-              console.log("difference in time", diffTime)
-
-        var remainder = diffTime % freqMin;
-              console.log(remainder, "remainder")
-
-        var tMinutesforTrain = freqMin - remainder;
-              console.log(tMinutesforTrain, "min for train")
-
-        var nextTrain = moment().add(tMinutesforTrain, "minutes").format("hh:mm a");
-              console.log("Next Train", nextTrain)
-
+  
       $("#trainNameDisplay").append(snapshot.val().trainName + "<hr>")
       $("#destination").append(snapshot.val().destination + "<hr>")
-      $("#trainFrequency").append(snapshot.val().freqMin + "<hr>")
-      $("#firstTrain").append(firstTimeConverted + "<hr>")
-      $("#minAway").append(tMinutesforTrain + "<hr>")
-      $("#nextArrival").append(moment(nextTrain).format("hh:mm") + "<hr>")
-     
+      $("#trainFrequency").append(snapshot.val().tFrequency + "<hr>")
+      $("#firstTrain").append(firstTrain + "<hr>")
+      $("#minAway").append(tMinutes + "<hr>")
+      $("#nextArrival").append(moment(nextTrain).format("HH:mm") + "<hr>");
+      
+
     // Clears all of the text-boxes
       $("#trainInput").val("");
       $("#destinationInput").val("");
       $("#freqInput").val("");
       $("#firstTrainInput").val("");
+});
 
-
-      //create removal button
+ //create removal button
       var remove = $("<button>");
-       $("#remove").append(remove)
-       $("#remove").append("<hr>")
-      remove.addClass("removeButton").attr("data-key")
+      $("#remove").append(remove + "<hr>");
+      remove.addClass("removeButton").attr("data-key");
       remove.text("Remove");
 
-$(".removeButton").on("click", function(event){
-  event.preventDefault();
-   $(this).closest("tr").remove(); 
-    alert("Deleted!");
+      $(".removeButton").on("click", function(event){
+      $(this).click(function(event){
+             event.preventDefault();
+            $("#trainRow").remove();
+            return false;
+        });
+           
+  });  
+
+
+
 
 
 
 });
-
-     
-  });
-
